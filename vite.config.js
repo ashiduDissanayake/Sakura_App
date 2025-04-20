3daf7c20ad9c13547bac27f988608394a7e7aeb9
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2025-04-21 00:42:17 +0530

    Add Docker support with Nginx for serving the application

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  publicPath: '/sakura/',
+  base: '/sakura/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
